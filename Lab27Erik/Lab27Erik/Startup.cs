d9a0ddf8cac9a721f37a2de6d7cd0d4dde0e0aa6
drkrieger <eplyushko@gmail.com>
2017-10-18 18:28:30 -0700

    controller for products done read for migration

--- Lab27Erik/Lab27Erik/Startup.cs
+++ Lab27Erik/Lab27Erik/Startup.cs
@@ -6,15 +6,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Lab27Erik.Models;
 
 namespace Lab27Erik
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<Lab27ErikContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Lab27ErikContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +39,13 @@ namespace Lab27Erik
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }

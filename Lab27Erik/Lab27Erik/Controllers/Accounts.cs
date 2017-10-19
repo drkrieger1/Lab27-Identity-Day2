8776cef9cd1304abeca6d3a2189cfa1c93917257
drkrieger <eplyushko@gmail.com>
2017-10-18 21:39:14 -0700

    login view model and register model maded with their views

---
+++ Lab27Erik/Lab27Erik/Controllers/Accounts.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27Erik.Controllers
+{
+    public class Accounts : Controller 
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
